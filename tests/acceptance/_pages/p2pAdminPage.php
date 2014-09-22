edb823d077a8b5f028efeaba19cc984fc46e6703
klapalme <kl.jr@aol.com>
2014-09-22 13:20:16 -0400

    p2p admin tests define reusable fields

--- tests/acceptance/_pages/p2pAdminPage.php
+++ tests/acceptance/_pages/p2pAdminPage.php
@@ -18,9 +18,46 @@ class P2pAdminPage
 
     public function __construct(AcceptanceTester $I)
     {
-        $this->acceptanceTester = $I;
-        $this->debugMode = '#edit-springboard-social-debug-mode';
-  
+      $this->acceptanceTester = $I;
+
+      //urls
+      $this->rulesUrl = 'springboard/p2p/rules';
+      $this->starterUrl = 'admin/springboard/p2p/starter';
+      $this->settingsUrl = 'springboard/p2p/settings';
+      $this->addCatUrl = 'springboard/add/p2p-category';
+
+      //fields
+      //settings fields
+      $this->messageArea = '//textarea[@name="login_message_area[value]"]';
+      $this->helpArea = '//textarea[@name="help_message_area[value]"]';
+      $this->donationEnable = '//input[@name="fundraiser_items[donation_form][enabled]"]';
+      $this->p2pEnable = '//input[@name="fundraiser_items[p2p_donation_form][enabled]"]';
+      $this->sbpZipEnable = '//input[@name="registration_fields[sbp_zip][enabled]"]';
+      $this->sbpStateEnable = '//input[@name="registration_fields[sbp_state][enabled]"]';
+      $this->sbpCountryEnable = '//input[@name="registration_fields[sbp_country][enabled]"]';
+      $this->sbpAddr2Enable = '//input[@name="registration_fields[sbp_address_line_2][enabled]"]';
+      $this->sbpAddrEnable = '//input[@name="registration_fields[sbp_address][enabled]"]';
+      $this->sbpCityEnable = '//input[@name="registration_fields[sbp_city][enabled]"]';
+
+      //add category fields
+      $this->catDesc = '//textarea[@name="body[und][0][value]"]';
+      $this->catTitle = '//input[@name="title"]';
+      $this->catBanner='//input[@name="files[field_p2p_campaign_banner_und_0]"]';
+      $this->orgIntro = '//textarea[@name="field_p2p_org_intro[und][0][value]"]';
+      $this->persIntro = '//textarea[@name="field_p2p_personal_intro[und][0][value]"]';
+      $this->persIntroEdit = '//input[@name="field_p2p_personal_intro_edit[und]"]';
+      $this->catImage = '//input[@name="files[field_p2p_category_image_und_0]"]';
+      $this->catImageThumb = '//input[@name="files[field_p2p_category_image_und_0]"]';
+      $this->catImageEdit = '//input[@name="field_p2p_images_edit[und]"]';
+      $this->catVid = '//input[@name="field_p2p_video_embed[und][0][video_url]"]';
+      $this->catVidEdit = '//input[@name="field_p2p_video_embed_edit[und]"]';
+     
+      //add p2p campaign
+      $this->catSelect = '//select[@name="field_p2p_category[und]"]';
+      //dom elements
+
+      $this->rulesDesc = '.rules-element-content .description';
+
     }
 
     /**
--- tests/acceptance/p2p/p2pAdminCept.php
+++ tests/acceptance/p2p/p2pAdminCept.php
@@ -10,44 +10,45 @@ $admin = new P2pAdminPage($I);
 $admin->enableFeature();
 
 // User has the ability to set the peer to peer login message
-$I->amOnPage('springboard/p2p/settings');
+$I->amOnPage($admin->settingsUrl);
 $I->wait(1);
 $I->see('P2P Login message', 'label');
-$I->fillField('#edit-login-message-area-value', 'My Custom Login Message');
+$I->fillField($admin->messageArea, 'My Custom Login Message');
 $I->see('P2P Help message', 'label');
-$I->fillField('#edit-help-message-area-value', 'My Custom Help Message');
+$I->fillField($admin->helpArea, 'My Custom Help Message');
 $I->click('#edit-submit');
-$I->see('My Custom Login Message', '#edit-login-message-area-value');
-$I->see('My Custom Help Message', '#edit-help-message-area-value');
+$I->see('My Custom Login Message', $admin->messageArea);
+$I->see('My Custom Help Message', $admin->helpArea);
 
 // A list of all fundraiser types and the forms of that type can be enabled for peer to peer fundraising
 
 $I->see('Peer to peer enabled form types', 'legend');
-$I->seeElement('#edit-fundraiser-items-donation-form-enabled');
-$I->seeElement('#edit-fundraiser-items-p2p-donation-form-enabled');
+$I->seeElement($admin->donationEnable);
+$I->seeElement($admin->p2pEnable);
 
 // User has the ability to choose which configured profile fields display on the registration page, excluding first, last, and email, which are always required.
 $I->see('Registration fields', 'legend');
-$I->seeElement('#edit-registration-fields-sbp-zip-enabled');
-$I->seeElement('#edit-registration-fields-sbp-state-enabled');
-$I->seeElement('#edit-registration-fields-sbp-country-enabled');
-$I->seeElement('#edit-registration-fields-sbp-address-line-2-enabled');
-$I->seeElement('#edit-registration-fields-sbp-address-enabled');
-$I->seeElement('#edit-registration-fields-sbp-city-enabled');
+$I->seeElement($admin->sbpZipEnable);
+$I->seeElement($admin->sbpStateEnable);
+$I->seeElement($admin->sbpCountryEnable);
+$I->seeElement($admin->sbpAddr2Enable);
+$I->seeElement($admin->sbpAddrEnable);
+$I->seeElement($admin->sbpCityEnable);
 
 // User has the ability to sort the order in which the fields appear on the registration page
 
 $I->dragAndDrop('//table[@id="registration-fields-table"]//tr[contains(@class, "draggable")][1]//a', '//table[@id="registration-fields-table"]//tr[contains(@class, "draggable")][2]//a');
 
 // User can make non-required profile fields required on the registration page
-
-$I->checkOption('#edit-registration-fields-sbp-state-enabled');
-$I->checkOption('#edit-registration-fields-sbp-state-required');
+$I->checkOption('//input[@name="registration_fields[sbp_state][enabled]"]');
+$I->checkOption('//input[@name="registration_fields[sbp_state][required]"]');
 $I->click('#edit-submit');
-$I->amOnPage('admin/springboard/p2p/starter');
+//generate dummy content
+$I->amOnPage($admin->starterUrl);
 $I->click('Create content');
 $I->wait(15);
 $I->logout();
+//check required fields
 $I->amOnPage('p2p/register?p2p_cid=11');
 $I->wait(1);
 $I->seeElement('.form-item-sbp-state-und .form-required');
@@ -61,17 +62,17 @@ $I->seeElement('.form-item-sbp-state-und .form-required');
 // User can configure an email for when an existing drupal user requests a password reset via the P2P UI.
 
 $I->login();
-$I->amOnPage('springboard/p2p/rules');
+$I->amOnPage($admin->rulesUrl);
 
-$I->see('rules_p2p_admin_email_private_campaign_approval', '.rules-element-content .description');
-$I->see('rules_p2p_password_reset_mail', '.rules-element-content .description');
-$I->see('rules_p2p_user_email_personal_campaign_creation', '.rules-element-content .description');
-$I->see('rules_p2p_user_email_private_campaign_approval_request', '.rules-element-content .description');
-$I->see('rules_p2p_user_email_private_campaign_approved', '.rules-element-content .description');
-$I->see('rules_p2p_user_email_private_campaign_rejected', '.rules-element-content .description');
-$I->see('rules_p2p_user_email_registration', '.rules-element-content .description');
-$I->see('rules_p2p_user_email_registration_private_campaign', '.rules-element-content .description');
-$I->see('rules_p2p_user_email_registration_public_campaign', '.rules-element-content .description');
+$I->see('rules_p2p_admin_email_private_campaign_approval', $admin->rulesDesc);
+$I->see('rules_p2p_password_reset_mail', $admin->rulesDesc);
+$I->see('rules_p2p_user_email_personal_campaign_creation', $admin->rulesDesc);
+$I->see('rules_p2p_user_email_private_campaign_approval_request', $admin->rulesDesc);
+$I->see('rules_p2p_user_email_private_campaign_approved', $admin->rulesDesc);
+$I->see('rules_p2p_user_email_private_campaign_rejected', $admin->rulesDesc);
+$I->see('rules_p2p_user_email_registration', $admin->rulesDesc);
+$I->see('rules_p2p_user_email_registration_private_campaign', $admin->rulesDesc);
+$I->see('rules_p2p_user_email_registration_public_campaign', $admin->rulesDesc);
 
 $I->click('Send user email after creating a personal campaign');
 $I->seeElement('#edit-settings');
@@ -79,25 +80,25 @@ $I->seeElement('#edit-settings');
 // Dashboard
 // User sees a list of configured peer to peer campaigns
 
-$I->amOnPage('springboard/p2p');
+$I->amOnPage(p2pAdminPage::$url);
 $I->see('Cross River Gorilla');
 // User has the ability to create a new peer to peer campaign
 $I->click('Create a new campaign');
 $I->see('Create Peer to Peer Campaign', 'H1');
 // User sees a list of configured peer to peer categories
-$I->amOnPage('springboard/p2p');
+$I->amOnPage(p2pAdminPage::$url);
 $I->see('Runs and Walks');
 // User has the ability to create a new peer to peer category
 $I->click('Create a new category');
 $I->see('Create Peer to Peer Category', 'H1');
 
 // User sees a list of personal campaigners that require approval
-$I->amOnPage('springboard/p2p');
+$I->amOnPage(p2pAdminPage::$url);
 $I->see('Users awaiting approval', 'H2');
 // User has the ability to approve or reject a campaigner from the dashboard
 
 // User sees a list of personal campaigns
-$I->amOnPage('springboard/p2p');
+$I->amOnPage(p2pAdminPage::$url);
 $I->see('Personal campaigns', 'H2');
 
 // Approval Queue
@@ -106,9 +107,7 @@ $I->see('Personal campaigns', 'H2');
 
 // Peer to Peer Category Creation
 // Only a permissioned user can create campaign categories
-$I->amOnPage('admin/people/create');
-$rid = $I->grabValueFrom('//label[normalize-space(text())="Springboard administrator"]/preceding-sibling::input');
-
+$rid = $I->getRid('Springboard administrator');
 $I->amOnPage('admin/people/permissions/' . $rid);
 $I->checkOption('#edit-' . $rid . '-create-p2p-category-content');
 $I->checkOption('#edit-' . $rid . '-create-p2p-campaign-content');
@@ -119,48 +118,51 @@ $I->checkOption('#edit-' . $rid . '-edit-any-p2p-campaign-content');
 $I->checkOption('#edit-' . $rid . '-edit-any-p2p-campaign-landing-content');
 $I->checkOption('#edit-' . $rid . '-edit-any-p2p-personal-campaign-content');
 $I->click('#edit-submit');  
-//$I->createUser('testp2p', 'testp2p@example.com', $rid);
+$I->createUser('testp2p', 'testp2p@example.com', $rid);
 $I->logout();
 $I->wait(4);
 $I->login('testp2p', 'testp2p');
 
 //User must provide a name, description and image when creating a new category
-$I->amOnPage('springboard/add/p2p-category');
-$I->seeElement('.form-item-title');
-$I->seeElement('.form-item-body-und-0-value .required');
+$I->amOnPage($admin->addCatUrl);
+$I->click('#edit-submit');
+$I->see('Name field is required');
+$I->see('Description field is required');
+$I->see('Category image field is required');
 
-$I->fillField('#edit-title', 'My Category Title');
-$I->fillField('#edit-body-und-0-value', 'This is a category description');
+$I->fillField($admin->catTitle, 'My Category Title');
+$I->fillField($admin->catDesc, 'This is a category description');
 // User can upload a donation form banner
 $I->click('Donation form');
-$I->attachFile('#edit-field-p2p-campaign-banner-und-0-upload', '1170x240.png');
+$I->attachFile($admin->catBanner, '1170x240.png');
 $I->click('Upload');
 
 // User can provide default content that can be used in campaigns and personal campaigns
 // User can set an organization introduction
 $I->click("Advanced");
 $I->see('Organization introduction', 'Label');
-$I->fillField('#edit-field-p2p-org-intro-und-0-value', 'My organization introduction');
+$I->fillField($admin->orgIntro, 'My organization introduction');
 
 // User can set a personal campaign introduction
 $I->click("Personal campaign introduction");
+$I->wait(1);
 $I->see("Personal campaign introduction", 'label');
-$I->fillField('#edit-field-p2p-personal-intro-und-0-value', 'My personal introduction');
+$I->fillField($admin->persIntro, 'My personal introduction');
 
 // User can specify if the personal campaigner can override the personal campaign introduction
-$I->checkOption('#edit-field-p2p-personal-intro-edit-und');
+$I->checkOption($admin->persIntroEdit);
 // User can upload personal campaign images
 $I->click("Media");
 //category
-$I->attachFile('#edit-field-p2p-category-image-und-0-upload', '400x240.png');
+$I->attachFile($admin->catImage, '400x240.png');
 //thumbnail;
-$I->attachFile('#edit-field-p2p-images-und-0-upload', '400x240.png');
+$I->attachFile($admin->catImageThumb, '400x240.png');
 // User can specify if the personal campaigner can override the images
-$I->checkOption('#edit-field-p2p-images-edit-und');
+$I->checkOption($admin->catImageEdit);
 // User can set a video embed url
-$I->fillField('#edit-field-p2p-video-embed-und-0-video-url', 'http://www.youtube.com/watch?v=6MMMN0VZmYw');
+$I->fillField($admin->catVid, 'http://www.youtube.com/watch?v=');
 // User can specify if the personal campaigner can override the video embed
-$I->checkOption('#edit-field-p2p-video-embed-edit-und');
+$I->checkOption($admin->catVidEdit);
 $I->click("#edit-submit");
 
 // Peer to Peer Campaign Creation
@@ -191,15 +193,16 @@ $I->see('Select a goal type.', '.error li');
 // The personal campaign images' overridable setting is inherited from the selected category
 // If the selected category has a video embed it is pre-populated
 // The video embed's overridable setting is inherited from the selected category
-$I->selectOption('//select[@name="field_p2p_category[und]"]', "My Category Title");
+
+$I->selectOption($admin->catSelect, "My Category Title");
 $I->wait('5');
 $I->click('Personal campaign defaults');
 $I->seeElement('//textarea[normalize-space(text())="My organization introduction"]');
 $I->click('Personal campaign introduction');
 $I->seeElement('//textarea[normalize-space(text())="My personal introduction"]');
-$I->seeCheckboxIsChecked('//input[@name="field_p2p_personal_intro_edit[und]"]');
+$I->seeCheckboxIsChecked($admin->persIntroEdit);
 $I->click('Media');
-$I->seeCheckboxIsChecked('//input[@name="field_p2p_images_edit[und]"]');
+$I->seeCheckboxIsChecked($admin->catImageEdit);
 $I->seeElement('.draggable .image-preview img');
 $I->seeElement('div.field-name-field-p2p-campaign-banner img');
 $I->see('This is a Youtube or Vimeo video URL');
