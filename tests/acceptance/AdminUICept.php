6377c3d8fb5ca21b5a6c806c26e9742bf5f90ead
Phillip Cave <phillip.cave@jacksonriver.com>
2014-10-03 16:33:40 -0400

    Adding more assertions to admin ui tests.

--- tests/acceptance/AdminUICept.php
+++ tests/acceptance/AdminUICept.php
@@ -17,7 +17,7 @@ $I->login('john', 'john');
 
 $I->seeInCurrentUrl('/springboard');
 $I->see('Administration', '.page-title');
-$I->cantSeeElement('.error');
+//$I->cantSeeElement('.error');
 
 // Main menu checks.
 $I->see('Donation Forms', 'a.dropdown-toggle');
@@ -39,21 +39,101 @@ $I->see('Springboard Version:', '.sb-version-info');
 
 $I->see('Springboard Notes', 'h2');
 
-// Problems start here.
 $I->click('Donation Forms');
 $I->seeInCurrentUrl('/springboard/donation-forms/all');
 $I->see('Donation Forms', '.page-title');
-
-/*
-$I->click('Donation Forms');
-$I->seeInCurrentUrl('/springboard/donation-forms/all');
-
-
 //$I->cantSeeElement('.error');
-//$I->see('Donation Forms', '.page-title');
-//$I->see('Donation Form', 'h2');
+$I->see('Donation Forms', '.page-title');
+$I->see('Donation Form', 'h2');
 $I->see('Create Donation Form', '.add-button');
 $I->see('View All Donation Forms', '.more-button');
-$I->see('Options', '.button');
+$I->see('Options', 'button');
+
+// Form view.
 $I->see('Internal Name', 'th');
-*/
+$I->see('Form Name', 'th');
+$I->see('Form ID', 'th');
+$I->see('Status', 'th');
+$I->see('Date Created', 'th');
+$I->see('Action', 'th');
+$I->see('Clone', 'td');
+$I->see('Edit', 'td');
+$I->see('View', 'td');
+
+$I->click('View all Donation Forms');
+$I->seeInCurrentUrl('/springboard/donation-forms/donation_form');
+$I->see('Donation Forms', '.page-title');
+$I->see('Internal Name', 'th');
+$I->see('Form Name', 'th');
+$I->see('Form ID', 'th');
+$I->see('Status', 'th');
+$I->see('Date Created', 'th');
+$I->see('Action', 'th');
+$I->see('Clone', 'td');
+$I->see('Edit', 'td');
+$I->see('View', 'td');
+$I->fillField('#edit-combine', 'NORESULT');
+$I->click('Go');
+$I->waitForElementNotVisible('.views-table', 30);
+$I->dontSeeElement('.views-table');
+$I->fillField('#edit-combine', 'Test');
+$I->click('Go');
+$I->waitForElementVisible('.views-table', 30);
+$I->see('Test Donation Form', 'td');
+
+$I->moveMouseOver('li.donationforms');
+$I->click('Create a Donation Form');
+$I->seeInCurrentUrl('/springboard/add/donation-form');
+$I->see('Create Donation Form', '.page-title');
+
+$I->moveMouseOver('li.donationforms');
+$I->click('Donation Reports');
+$I->seeInCurrentUrl('/springboard/reports/donations');
+$I->see('Donations', '.page-title');
+$I->seeElement('#views-exposed-form-sbv-donations-page');
+
+// Forms menu item.
+$I->click('Forms');
+$I->seeInCurrentUrl('/springboard/forms/all');
+$I->see('Forms', '.page-title');
+$I->moveMouseOver('li.forms');
+$I->click('View All Forms');
+$I->seeInCurrentUrl('/springboard/forms/all');
+
+// Asset Library
+$I->click('Asset Library');
+$I->seeInCurrentUrl('/springboard/asset-library');
+$I->see('Templates', '.page-title');
+$I->see('Page Wrapper', 'h2');
+$I->see('Create Page Wrapper', 'a');
+$I->see('View all Page Wrappers', 'a');
+$I->see('Email Wrapper', 'h2');
+$I->see('Create Email Wrapper', 'a');
+$I->see('View all Email Wrappers', 'a');
+
+$I->moveMouseOver('li.assetlibrary');
+$I->click('Email Templates');
+$I->seeInCurrentUrl('/springboard/asset-library/email_wrapper');
+$I->see('Email Wrappers', '.page-title');
+$I->see('Title', 'th');
+$I->see('Status', 'th');
+$I->see('Date Created', 'th');
+$I->see('Action', 'th');
+$I->see('View', 'td a');
+$I->see('Edit', 'td a');
+$I->see('Clone', 'td a');
+$I->see('Delete', 'td a');
+
+$I->moveMouseOver('li.assetlibrary');
+$I->click('Page Wrappers');
+$I->seeInCurrentUrl('/springboard/asset-library/page_wrapper');
+$I->see('Page Wrappers', '.page-title');
+
+$I->click('Marketing & Analytics');
+$I->seeInCurrentUrl('/springboard/marketing-analytics');
+$I->see('Marketing & Analytics', '.page-title');
+$I->see('Source Codes', 'a');
+$I->see('Multivariate Testing', 'a');
+
+$I->moveMouseOver('li.reports');
+
