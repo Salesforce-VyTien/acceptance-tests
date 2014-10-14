c91d6dd7cefb4bbe9b42ad62ecfe4f7cb7542087
Phillip Cave <phillip.cave@jacksonriver.com>
2014-10-13 20:39:32 -0400

    Testing the contact report.

--- tests/acceptance/AdminUIReportsCept.php
+++ tests/acceptance/AdminUIReportsCept.php
@@ -4,6 +4,7 @@
 $I = new \AcceptanceTester\SpringboardSteps($scenario);
 $I->wantTo('use the Springboard Admin UI to view reports');
 
+// Make 5 donations to fill the report.
 for ($i = 0; $i <= 4; $i++) {
   $defaults = array(
     'email' => 'bob' . $i . '@example.com',
@@ -17,16 +18,52 @@ $I->wantTo('view the donation report.');
 $I->login();
 $I->seeInCurrentUrl('/springboard');
 
+// Check the contact report.
+$I->wantTo('view the contact report.');
+$I->moveMouseOver('li.reports');
+$I->click('Contacts');
+$I->seeInCurrentUrl('/springboard/reports/contacts');
+$headers = array(
+  'Uid',
+  'First name',
+  'Last name',
+  'E-mail',
+  'Address',
+  'City',
+  'State',
+  'Zip',
+  'Country',
+  'Created',
+  'Action',
+);
+
+foreach($headers as $header) {
+  $I->canSee($header, 'th');
+}
+
+for ($i = 0; $i <= 4; $i++) {
+  $I->canSee('Bob' . $i . '@Example.Com', 'a');
+}
+
+// Check the donation report.
 $I->moveMouseOver('li.reports');
 $I->click('Donations');
-$I->canSee('Order ID', 'th');
-$I->canSee('Date', 'th');
-$I->canSee('Amount', 'th');
-$I->canSee('Name', 'th');
-$I->canSee('E-mail', 'th');
-$I->canSee('Form', 'th');
-$I->canSee('Order status', 'th');
-$I->canSee('Actions', 'th');
+$I->seeInCurrentUrl('/springboard/reports/donations');
+
+$headers = array(
+  'Order ID',
+  'Date',
+  'Amount',
+  'Name',
+  'E-mail',
+  'Form',
+  'Order status',
+  'Actions',
+);
+
+foreach($headers as $header) {
+  $I->canSee($header, 'th');
+}
 
 // The 5 donations made previously should be in the report.
 for ($i = 0; $i <= 4; $i++) {
