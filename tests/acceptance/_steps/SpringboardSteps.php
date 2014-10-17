e44bcd1f4a16424d489a606121e11ae6fafedb7a
David Barbarisi <davidbarbarisi@gmail.com>
2014-10-17 11:45:00 -0400

    Added settings that can be overridden for making a donation, and added a function to make multiple random donations.

--- tests/acceptance/_steps/SpringboardSteps.php
+++ tests/acceptance/_steps/SpringboardSteps.php
@@ -6,19 +6,31 @@ class SpringboardSteps extends \AcceptanceTester\DrupalSteps
     public function makeADonation(array $details = array(), $recurs = FALSE)
     {
         $defaults = array(
+            'ask' => '10',
+            'first' => 'John',
+            'last' => 'Tester',
             'email' => 'bob@example.com',
+            'address' => '',
+            'address2' => '',
+            'city' => 'Washington',
+            'state' => 'DC',
+            'zip' => '20036',
+            'country' => 'United States',
+            'number' => '4111111111111111',
+            'year' => date('Y', strtotime('+ 1 year')),
+            'month' => 'January',
+            'cvv' => '666',
         );
 
         $settings = array_merge($defaults, $details);
 
         $I = $this;
 
-        // @todo Use custom settings.
-        $I->selectOption(\DonationFormPage::$askAmountField, '10');
-        $I->fillInMyName();
+        $I->selectOption(\DonationFormPage::$askAmountField, $settings['ask']);
+        $I->fillInMyName($settings['first'], $settings['last']);
         $I->fillField(\DonationFormPage::$emailField, $settings['email']);
-        $I->fillInMyAddress();
-        $I->fillInMyCreditCard();
+        $I->fillInMyAddress($settings['address'], $settings['address2'], $settings['city'], $settings['state'], $settings['zip'], $settings['country']);
+        $I->fillInMyCreditCard($settings['number'], $settings['year'], $settings['month'], $settings['cvv']);
 
         if ($recurs) {
             $I->selectOption(\DonationFormPage::$recursField, 'recurs');
@@ -100,5 +112,51 @@ class SpringboardSteps extends \AcceptanceTester\DrupalSteps
         $I->fillField('#edit-confirmation-value', $pageContent);
         $I->selectOption('confirmation[format]', 'full_html');
         $I->click('Save configuration');
+    }
+
+    /**
+     * Make multiple donations with random info.
+     *
+     * @param string $path
+     *   The path of the donation form. For example, '/node/2'.
+     * @param int $numberOfDonations
+     *   How many donations to make.
+     */
+    public function makeMultipleDonations($path, $numberOfDonations = 10) {
+        $I = $this;
+        // Used in combination with an iterator number to create a unique email address on each donation.
+        $request_time = strtotime('now');
+
+        $I->am('a donor');
+        $I->wantTo('donate.');
+
+        $asks = array('10', '20', '50', '100');
+        $firsts = array('Alice', 'Tom', 'TJ', 'Phillip', 'David', 'Shaun', 'Ben', 'Jennie', 'Sheena', 'Danny', 'Allen', 'Katie', 'Jeremy', 'Julia', 'Kate', 'Misty', 'Pat', 'Jenn', 'Joel', 'Katie', 'Matt', 'Meli', 'Jess');
+        $lasts = array('Hendricks', 'Williamson', 'Griffen', 'Cave', 'Barbarisi', 'Brown', 'Clark', 'Corman', 'Donnelly', 'Englander', 'Freeman', 'Grills', 'Isett', 'Kulla-Mader', 'McKenney', 'McLaughlin', 'O\'Brien', 'Olivia', 'Rothschild', 'Shaw', 'Thomas', 'Trumbo', 'Walls');
+        $numbers = array('4111111111111111');
+        $months = cal_info(0)['months'];
+
+        for ($iterator = 0; $iterator < $numberOfDonations; $iterator++) {
+            $defaults = array(
+                'ask' => $asks[array_rand($asks)],
+                'first' => $firsts[array_rand($firsts)],
+                'last' => $lasts[array_rand($lasts)],
+                'email' => 'test_' . $iterator . '_' . $request_time . '@example.com',
+                'address' => '1234 Main St',
+                'address2' => '',
+                'city' => 'Washington',
+                'state' => 'DC',
+                'zip' => '20036',
+                'country' => 'United States',
+                'number' => $numbers[array_rand($numbers)],
+                'year' => date('Y', strtotime('+ ' . ($i + 1) % 14 . ' years')),
+                'month' => $months[array_rand($months)],
+                'cvv' => rand(100, 999),
+            );
+
+            $recurring = ($iterator % 2) ? TRUE : FALSE;
+            $I->amOnPage($path);
+            $I->makeADonation($defaults, $recurring);
+        }
     }
 }
