b47f78d0d3fc917ef19311290ae04fee29efc59b
klapalme <kl.jr@aol.com>
2014-10-23 12:05:46 -0400

    add a 'no_populate' test group which allows db insertions during tests by altering the default db config

---
+++ tests/_groups/No_populateGroup.php
@@ -0,0 +1,33 @@
+<?php
+
+
+use \Codeception\Event\TestEvent;
+/**
+* Group class is Codeception Extension which is allowed to handle to all internal events.
+* This class itself can be used to listen events for test execution of one particular group.
+* It may be especially useful to create fixtures data, prepare server, etc.
+*
+* INSTALLATION:
+*
+* To use this group extension, include it to "extensions" option of global Codeception config.
+*/
+
+class No_populateGroup extends \Codeception\Platform\Group
+{
+    public static $group = 'no_populate';
+
+    public function _before(TestEvent $e)
+    {
+      exit();
+      $test = $e->getTest();
+      // get the Db module
+      $db = $this->getModule('Db');
+      $db->_reconfigure(array('populate' => FALSE));
+      $db->_initialize();
+      $db->_before($test);
+    }
+
+    public function _after(TestEvent $e)
+    {
+    }
+}
