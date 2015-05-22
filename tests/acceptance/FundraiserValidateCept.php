85486bbf1184d4026f33410d8d9d5fdbc8678a10
Phillip Cave <phillip.cave@jacksonriver.com>
2015-05-22 16:16:32 -0400

    Change test up to deal with timing issues.

--- tests/acceptance/FundraiserValidateCept.php
+++ tests/acceptance/FundraiserValidateCept.php
@@ -7,36 +7,41 @@ $title = 'fundraiser validation test ' . time();
 // Fill out fundraiser donation page form
 $I->amOnPage(DonationFormPage::$URL);
 $I->fillField(DonationFormPage::$otherAmountField, '120');
+//click on neutral space to remove focus from the last filled element
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-donation-other-amount.valid');
 $I->fillField(DonationFormPage::$firstNameField, 'Miles');
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-donor-information-first-name.valid');
 $I->fillField(DonationFormPage::$lastNameField, 'Davis');
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-donor-information-last-name.valid');
 $I->fillField(DonationFormPage::$emailField, 'md@example.com');
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-donor-information-mail.valid');
 $I->fillField(DonationFormPage::$addressField, '10 Fusion Drive');
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-billing-information-address.valid');
 $I->fillField(DonationFormPage::$addressField2, 'Suite Cool');
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-billing-information-address-line-2.valid');
 $I->fillField(DonationFormPage::$cityField, 'Jazzville');
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-billing-information-city.valid');
 $I->selectOption(DonationFormPage::$stateField, 'NY');
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-billing-information-state.valid');
 $I->selectOption(DonationFormPage::$countryField, 'US');
 $I->fillField(DonationFormPage::$zipField, '12345');
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-billing-information-zip.valid');
 $I->fillField(DonationFormPage::$creditCardNumberField, '4111111111111111');
+$I->click('#webform-client-form-2');
+$I->seeElement('#edit-submitted-payment-information-payment-fields-credit-card-number.valid');
 $I->selectOption(DonationFormPage::$creditCardExpirationMonthField, '6');
 $I->selectOption(DonationFormPage::$creditCardExpirationYearField, '2015');
 $I->fillField(DonationFormPage::$CVVField, '123');
-
-//click on neutral space to remove focus from the last filled element
 $I->click('#webform-client-form-2');
-
-$I->wait(2);
-
-// check for validation classes
-$I->seeElement('#edit-submitted-donation-other-amount.valid');
-$I->seeElement('#edit-submitted-donor-information-first-name.valid');
-$I->seeElement('#edit-submitted-donor-information-last-name.valid');
-$I->seeElement('#edit-submitted-donor-information-mail.valid');
-$I->seeElement('#edit-submitted-billing-information-address.valid');
-$I->seeElement('#edit-submitted-billing-information-address-line-2.valid');
-$I->seeElement('#edit-submitted-billing-information-city.valid');
-$I->seeElement('#edit-submitted-billing-information-state.valid');
-$I->seeElement('#edit-submitted-billing-information-zip.valid');
-$I->seeElement('#edit-submitted-payment-information-payment-fields-credit-card-number.valid');
 $I->seeElement('#edit-submitted-payment-information-payment-fields-credit-card-cvv.valid');
 
 // fill fields with invalid values and check validation
