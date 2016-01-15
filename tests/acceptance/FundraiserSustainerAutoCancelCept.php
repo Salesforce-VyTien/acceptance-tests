9fa19733124f26ab5c6442ed2b51f348431dcab2
jestep <josh.estep@mediacurrent.com>
2016-01-15 13:31:59 -0500

    Completed first draft of FundraiserSustainerAutoCancelCept.php

--- tests/acceptance/FundraiserSustainerAutoCancelCept.php
+++ tests/acceptance/FundraiserSustainerAutoCancelCept.php
@@ -2,17 +2,30 @@
 $I = new \AcceptanceTester\SpringboardSteps($scenario);
 $I->wantTo('Test auto-cancel of remaining, recurring payments when X number of failed payments is reached.');
 
+$master_did = 1; // We know the master_did will be 1 based on contents of the DB dump:
+
 $title = 'fundraiser recurring payments auto-cancel test ' . time();
 
 // Log in to Drupal:
 $I->am('admin');
 $I->login();
 
+// Update the Secure Key Path:
+$I->amOnPage('admin/config/system/encrypt');
+$I->fillField('#edit-encrypt-secure-key-path', 'sites/default/secure');
+$I->click('#edit-submit');
+
 // Ensure Fundraiser Sustainers module is enabled, as it implement the auto-cancellation feature:
 $I->enableModule('Fundraiser Sustainers');
 
-// Ensure Auto-cancel is enabled
-$I->haveInDatabase('variable', array('name' => 'fundraiser_recurring_autocancel_enabled', 'value' => 'i:1;'));
+// Ensure Auto-cancel is enabled:
+$I->amOnPage('admin/config/system/fundraiser');
+$I->wait(2);
+$I->click('Fundraiser sustainers');
+$I->wait(1);
+$I->checkOption('#edit-fundraiser-recurring-autocancel-enabled');
+$I->wait(1);
+$I->seeCheckboxIsChecked('input#edit-fundraiser-recurring-autocancel-enabled');
 
 // Get the number of payment failures (default is 3) to trigger auto-cancellation of any remaining payments:
 $auto_cancel_failure_limit = $I->grabFromDatabase('variable', 'value', array('name' => 'fundraiser_recurring_autocancel_threshold'));
@@ -65,28 +78,52 @@ codecept_debug('single_payment_failure_limit = "' . $single_payment_failure_limi
 
 // Based on the default number of failures, set card expiration date so that 2 future payments
 // end up getting auto-cancelled after auto-cancellation is triggered:
-/* $month = 6; $year = 2017;
+$year = date('Y');
+$month = date('n') + $auto_cancel_failure_limit + 2;
+if ($month > 12) {
+  $month = 12 - $month;
+  $year++;
+}
 $I->selectOption(DonationFormPage::$creditCardExpirationMonthField, $month);
 $I->selectOption(DonationFormPage::$creditCardExpirationYearField, $year);
-*/
+
+// Make it a recurring donation:
+$I->click('#edit-submitted-payment-information-recurs-monthly-1');
 
 // Submit the donation and ensure it submits successfully:
+$I->click('#edit-submit');
 
-// View the Commerce orders list and get the master order ID from the top item's link:
+// Confirm the form submitted correctly:
+$I->seeInMessages('Donation was successfully processed.');
+$I->seeInDatabase('commerce_order', array('order_id' => $master_did, 'status' => 'payment_received'));
 
-// Open the recurring payments edit page:
+// Open the "Recurring Donation Set -> Edit donation set" tab
 
 // Trigger cancellation:
-/* for ($i = 0; $i < $auto_cancel_failure_limit; $i++) {
+$current_did = $master_did + 1;
+for ($i = 0; $i < $auto_cancel_failure_limit; $i++) {
   // Advance the next payment until a failed state is expected to be seen:
   for ($j = 0; $j < $single_payment_failure_limit; $j++) {
-    // Confirm each failure is achieved as expected:
-    // Confirm the associated Salesforce sync update is inserted into the salesforce_sync table:
+    codecept_debug('Advancing charge ' . ($i + 1) . ' - ' . ($j + 1));
+    $I->amOnPage('fundraiser_sustainers/ffwd/' . $current_did . '?destination=/');
+    $I->amOnPage('admin/reports/status/run-cron');
   }
-} */
+
+  // Confirm a failed state was achieved:
+  $I->seeInMessages('Donation transaction failed.');
+  $I->seeInDatabase('commerce_order', array('order_id' => $current_did, 'status' => 'failed'));
+
+  // Confirm the associated Salesforce sync update is inserted into the salesforce_sync table:
+
+  $current_did++;
+} 
 
 // Confirm the 2 remaining payments have a status of auto-cancelled:
+$I->seeInMessages('Your payment #' . $current_did . ' has been canceled.');
+$I->seeInDatabase('commerce_order', array('order_id' => $current_did, 'status' => 'auto_canceled'));
+$I->seeInMessages('Your payment #' . ($current_did + 1) . ' has been canceled.');
+$I->seeInDatabase('commerce_order', array('order_id' => $current_did + 1, 'status' => 'auto_canceled'));
 
 
-
+$I->wait(1);
 
