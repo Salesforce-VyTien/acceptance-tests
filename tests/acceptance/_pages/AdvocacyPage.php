d14363384504b7988addc8ab30c2daba98537869
klapalme <kl.jr@aol.com>
2016-01-22 12:28:22 -0500

    add configuration for scrutinizer and enable one advocacy test

--- tests/acceptance/_pages/AdvocacyPage.php
+++ tests/acceptance/_pages/AdvocacyPage.php
@@ -4,38 +4,53 @@ class AdvocacyPage
 {
 
   /**
-  * Declare UI map for this page here. CSS or XPath allowed.
-  * public static $usernameField = '#username';
-  * public static $formSubmitButton = "#mainForm input[type=submit]";
-  */
+   * Declare UI map for this page here. CSS or XPath allowed.
+   * public static $usernameField = '#username';
+   * public static $formSubmitButton = "#mainForm input[type=submit]";
+   */
 
 
   //urls
   public static $settingsPage = 'admin/config/services/advocacy';
 
-   /**
-     * @var AcceptanceTester;
-     */
-    protected $acceptanceTester;
+  /**
+   * @var AcceptanceTester;
+   */
+  protected $acceptanceTester;
 
-    public function __construct(AcceptanceTester $I)
-    {
-      $this->acceptanceTester = $I;
+  public function __construct(AcceptanceTester $I)
+  {
+    $this->acceptanceTester = $I;
 
-    }
+  }
 
-    /**
-     * @return a thing.
-     */
-    public static function of(AcceptanceTester $I)
-    {
-        return new static($I);
-    }
+  /**
+   * @return a thing.
+   */
+  public static function of(AcceptanceTester $I)
+  {
+    return new static($I);
+  }
 
-    function configureAdvocacy() {
+  function configureAdvocacy() {
+    if (empty($_ENV['springboard_advocacy_server_url'])) {
       $config = \Codeception\Configuration::config();
       $settings = \Codeception\Configuration::suiteSettings('acceptance', $config);
-      $I = $this->acceptanceTester;
+    }
+    else {
+      // Scrutinizer env vars.
+      $settings['Advocacy'] = array(
+        'springboard_advocacy_server_url' => $_ENV['springboard_advocacy_server_url'],
+        'springboard_advocacy_client_id' => $_ENV['springboard_advocacy_client_id'],
+        'springboard_advocacy_client_secret' => $_ENV['springboard_advocacy_client_secret'],
+        'springboard_advocacy_smarty_authid' => $_ENV['springboard_advocacy_smarty_authid'],
+        'springboard_advocacy_smarty_authtoken' => $_ENV['springboard_advocacy_smarty_authtoken'],
+        'social_action_twitter_consumer_key' => $_ENV['social_action_twitter_consumer_key'],
+        'social_action_twitter_consumer_secret' => $_ENV['social_action_twitter_consumer_secret'],
+        'springboard_advocacy_test_email' => $_ENV['springboard_advocacy_test_email'],
+      );
+    }
+    $I = $this->acceptanceTester;
 
     foreach ($settings['Advocacy'] as $key => $value) {
       $I->haveInDatabase('variable', array('name' => $key, 'value' => $value));
@@ -43,8 +58,15 @@ class AdvocacyPage
   }
 
   function twitterLogin() {
-    $config = \Codeception\Configuration::config();
-    $settings = \Codeception\Configuration::suiteSettings('acceptance', $config);
+    if (empty($_ENV['twitter_name'])) {
+      $config = \Codeception\Configuration::config();
+      $settings = \Codeception\Configuration::suiteSettings('acceptance', $config);
+    }
+    else {
+      // Scrutinizer env vars.
+      $settings['Twitter'] = array('name' => $_ENV['twitter_name'], 'pass' => $_ENV['twitter_pass']);
+    }
+
     $I = $this->acceptanceTester;
     $I->fillField('#username_or_email', $settings['Twitter']['name']);
     $I->fillField('#password', $settings['Twitter']['pass']);
