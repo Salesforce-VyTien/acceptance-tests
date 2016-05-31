61fefcef1785831b1fcc1c39a41e547fafed8616
klapalme <kl.jr@aol.com>
2016-05-31 11:26:54 -0400

    date mode

--- tests/acceptance/fundraiser/FundraiserDateModeCept.php
+++ tests/acceptance/fundraiser/FundraiserDateModeCept.php
@@ -22,6 +22,7 @@ else {
   $today = $today + 1;
 }
 
+// Configure date mode.
 $I->amOnPage('admin/config/system/fundraiser/date-mode');
 $I->checkOption('#edit-fundraiser-date-mode-set-date-mode');
 $I->selectOption('#edit-fundraiser-date-mode-set-dates', $today);
@@ -31,14 +32,30 @@ $I->selectOption('#edit-fundraiser-date-mode-set-seconds', 22);
 $I->wait('4');
 $I->click('Save configuration');
 
+// Check if date mode updated existing series correctly.
 $I->amOnPage('springboard/donations/1/recurring');
 $new_date = '/' . $today . '/' . date('y');
 $I->see($new_date);
 
+// Set date mode to a different date.
 $I->amOnPage('admin/config/system/fundraiser/date-mode');
 $I->selectOption('#edit-fundraiser-date-mode-set-dates', $today + 1);
 $I->click('Save configuration');
 
+// Check if date mode updated existing series correctly.
 $I->amOnPage('springboard/donations/1/recurring');
 $new_date = '/' . ($today + 1) . '/' . date('y');
 $I->see($new_date);
+
+// Unset date mode
+$today = date('j');
+$I->amOnPage('admin/config/system/fundraiser/date-mode');
+$I->unCheckOption('#edit-fundraiser-date-mode-set-date-mode');
+$I->wait('4');
+$I->click('Save configuration');
+
+$I->amOnPage('springboard/donations/1/recurring');
+$original_date = '/' . ($today) . '/' . date('y');
+$I->see($original_date);
+
+//@todo sustainer processing, salesforce
