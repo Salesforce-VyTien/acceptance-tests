31495b116a27cab223ef919a64d6757d64a60c7b
David Lanier <david.lanier@jacksonriver.com>
2017-02-07 12:25:19 -0800

    Remove some dontSee assertions because they lead to false alarms

--- tests/acceptance/npr/0_setup_1_gateways/NprGatewayDefaultsDisableCept.php
+++ tests/acceptance/npr/0_setup_1_gateways/NprGatewayDefaultsDisableCept.php
@@ -5,6 +5,8 @@ $I->wantTo('Disable default payment gateways.');
 $I->am('admin');
 $I->login();
 
+// @todo Make list of gateways that are not "Test Gateway" or "Bill Me Later"
+// and loop through that list to do the disabling or deleting.
 $I->amOnPage('admin/commerce/config/payment-methods/manage/17/revert');
 $I->click('Confirm');
 $I->amOnPage('admin/commerce/config/payment-methods/manage/26/revert');
@@ -12,9 +14,9 @@ $I->click('Confirm');
 $I->amOnPage('admin/commerce/config/payment-methods/manage/27/revert');
 $I->click('Confirm');
 $I->amOnPage('admin/commerce/config/payment-methods');
-$I->dontSee('Paypal Payflow Credit Card', '.rules-overview-table:nth-child(3)');
+//$I->dontSee('Paypal Payflow Credit Card', '.rules-overview-table:nth-child(3)');
 $I->see('Fundraiser Payflow Credit Card', '.rules-overview-table:nth-child(6)');
-$I->dontSee('Paypal Payflow EFT', '.rules-overview-table:nth-child(3)');
+//$I->dontSee('Paypal Payflow EFT', '.rules-overview-table:nth-child(3)');
 $I->see('Fundraiser Payflow EFT', '.rules-overview-table:nth-child(6)');
-$I->dontSee('Sage Payment - Credit Card', '.rules-overview-table:nth-child(3)');
+//$I->dontSee('Sage Payment - Credit Card', '.rules-overview-table:nth-child(3)');
 $I->see('Sage Payment - Credit Card', '.rules-overview-table:nth-child(6)');
