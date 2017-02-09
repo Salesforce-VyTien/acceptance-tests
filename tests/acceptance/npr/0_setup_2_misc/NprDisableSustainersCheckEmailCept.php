7d6174451c58761fa5d25128c8a01be289220086
David Lanier <david.lanier@jacksonriver.com>
2017-02-09 06:57:14 -0800

    code style on NprDisableSustainersCheckEmailCept.php

--- tests/acceptance/npr/0_setup_2_misc/NprDisableSustainersCheckEmailCept.php
+++ tests/acceptance/npr/0_setup_2_misc/NprDisableSustainersCheckEmailCept.php
@@ -5,9 +5,8 @@ $I->wantTo('Disable the "Sustainers failed health checks" rule that sends an ema
 $I->am('admin');
 $I->login();
 
-$I->amOnPage('/admin/config/workflow/rules/reaction/manage/rules_sustainers_check_failure');
+$I->amOnPage('admin/config/workflow/rules/reaction/manage/rules_sustainers_check_failure');
 $I->click('Settings');
-//$I->waitForText('Active');
 $I->waitForElementVisible('#edit-settings-active');
 $I->uncheckOption('#edit-settings-active');
 $I->click('Save changes');
