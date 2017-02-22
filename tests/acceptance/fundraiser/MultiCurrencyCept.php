22a2bc984645cc9c82430a4ea4899ab95ca47a8a
klapalme <kl.jr@aol.com>
2017-02-22 13:58:44 -0500

    fix id

--- tests/acceptance/fundraiser/MultiCurrencyCept.php
+++ tests/acceptance/fundraiser/MultiCurrencyCept.php
@@ -15,7 +15,7 @@ $I->configureEncrypt();
 $I->amOnPage(MultiCurrencyPage::$URL);
 $I->selectOption(MultiCurrencyPage::$select, MultiCurrencyPage::$eur);
 $I->click(MultiCurrencyPage::$adminFieldset);
-$I->waitForElementVisible('#uniform-edit-commerce-default-currency', 10);
+$I->waitForElementVisible('#edit-commerce-default-currency', 10);
 $I->wait(3);
 $I->checkOption(MultiCurrencyPage::$eur);
 $I->click(MultiCurrencyPage::$configSave);
