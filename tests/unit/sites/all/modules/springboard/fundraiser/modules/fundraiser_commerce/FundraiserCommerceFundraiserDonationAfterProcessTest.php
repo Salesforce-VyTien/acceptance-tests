c407f9aac4ecaf4682c90cea1230728d88e15298
Jeremy Isett <jeremyisett@gmail.com>
2017-04-27 15:47:31 -0400

    SB1824 - Adjust test to match latest code.

--- tests/unit/sites/all/modules/springboard/fundraiser/modules/fundraiser_commerce/FundraiserCommerceFundraiserDonationAfterProcessTest.php
+++ tests/unit/sites/all/modules/springboard/fundraiser/modules/fundraiser_commerce/FundraiserCommerceFundraiserDonationAfterProcessTest.php
@@ -43,18 +43,20 @@ class FundraiserCommerceFundraiserDonationAfterProcessTest extends UnitBaseTest 
     $this::createMockFunctionDefinition('commerce_order_load');
     $this::createMockFunctionDefinition('user_load');
     $this::createMockFunctionDefinition('db_query');
+    $this::createMockFunctionDefinition('commerce_cardonfile_load');
   }
 
   public function testFundraiserCommerceFundraiserDonationAfterProcess() {
     // For stubbing the db_query we pass in the expected sql and array of arguments.
-    $dbquerysql = 'SELECT DISTINCT(fs.master_did) FROM {fundraiser_sustainers} fs LEFT JOIN {fundraiser_donation} fd ON fd.did = fs.did WHERE fd.card_id = :card_id';
+    $dbquerysql = 'SELECT DISTINCT(fs.master_did), fd.did FROM {fundraiser_sustainers} fs LEFT JOIN {fundraiser_donation} fd ON fd.did = fs.did WHERE fd.card_id = :card_id';
     $dbquery_args = array(':card_id' => 13);
     // We pass in the test class to stub for setting the $fetchAllRan to true.
     $db_mock = new FundraiserCommerceFundraiserDonationAfterProcessDbMock($this);
     $this::stub('db_query', $db_mock, array($dbquerysql, $dbquery_args));
 
     $this::stub('commerce_order_load', $this->commerce_order);
-    $this::stub('user_load', array());
+    $this::stub('user_load', TRUE);
+    $this::stub('commerce_cardonfile_load', TRUE);
 
     // Exercise the fundraiser_commerce_fundraiser_donation_after_process() function.
     fundraiser_commerce_fundraiser_donation_after_process($this->fundraiser_donation);
@@ -84,7 +86,7 @@ class FundraiserCommerceFundraiserDonationAfterProcessDbMock {
     $this->test = func_get_args()[0];
   }
 
-  public function fetchAll() {
+  public function fetchAllAssoc() {
     $this->test->fetchAllRan = TRUE;
     return array();
   }
